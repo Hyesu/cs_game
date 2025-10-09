d3f2160a5cfb34cc281ff36e9969c183c1591167
hotspam <accrmfrn84@gmail.com>
2025-10-09 18:36:30 +0900

    셀 매트릭스 - 테스트 및 틀 작성

---
+++ HFin/Cell/HCellMatrix2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace HFin.Cell
+{
+    public class HCellMatrix2D<T> where T : IHCell
+    {
+        private readonly int _cellSize;
+        private readonly int _numDirection;
+
+        public int CellCount => 0;
+
+        public HCellMatrix2D(int cellSize, int numDirection)
+        {
+            _cellSize = cellSize;
+            _numDirection = numDirection;
+        }
+
+        public void Add(IList<T> cells)
+        {
+        }
+
+        public void Clear()
+        {
+        }
+
+        public ImmutableArray<T> GetAllAdjacent(T cell)
+        {
+            return ImmutableArray<T>.Empty;
+        }
+    }   
+}
---
+++ HFin/Cell/IHCell.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace HFin.Cell
+{
+    public interface IHCell
+    {
+        Vector3 GetCenter();
+        Vector2 GetCenter2D();
+    }   
+}
